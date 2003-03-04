1b5dbdc2b05b962a84fb9f7b0edeebdca93027ee
Cato Olsen <>
2003-03-04 15:27:39 +0000

    - The session store timeout handler/task. Doesn't do much as of now.

---
+++ moria/src/no/feide/moria/SessionStoreTask.java
@@ -0,0 +1,26 @@
+package no.feide.moria;
+
+import java.util.TimerTask;
+import java.util.logging.Logger;    
+
+
+/**
+ * Represents a periodic task to manipulate the session store.
+ */
+public class SessionStoreTask
+extends TimerTask {
+    
+    /** Used for logging. */
+    private static Logger log = Logger.getLogger(SessionStoreTask.class.toString());
+    
+    /**
+     * Do something clever.
+     **/
+    public void run() {
+        log.finer("run()");
+        
+        // TODO: Add session store maintenance operations here.
+        //log.info("Cleaning up SessionStore - not.");
+    }    
+    
+}
