26231ec4d41bcbf8da38860a798d5aad84f91238
Cato Olsen <>
2003-03-05 12:31:30 +0000

    - Logging removed.

--- moria/src/no/feide/moria/BackendException.java
+++ moria/src/no/feide/moria/BackendException.java
@@ -6,8 +6,6 @@
 
 package no.feide.moria;
 
-import java.util.logging.Logger;
-
 /**
  * Used to throw exceptions from the backend.
  * @author Cato Olsen
@@ -15,16 +13,12 @@ import java.util.logging.Logger;
 public class BackendException
 extends java.lang.Exception {
     
-    /** Used for logging. */
-    private static Logger log = Logger.getLogger(User.class.toString());
-    
     /**
      * Basic constructor.
      * @param message Exception message.
      */
     public BackendException(String message) {
         super(message);
-        log.finer("BackendException(String)");
     }
     
     /**
@@ -34,7 +28,6 @@ extends java.lang.Exception {
      */
     public BackendException(Throwable cause) {
         super(cause);
-        log.finer("BackendException(Throwable)");
     }
     
     /**
@@ -44,6 +37,5 @@ extends java.lang.Exception {
      */
     public BackendException(String message, Throwable cause) {
         super(message, cause);
-        log.finer("BackendException(String, Throwable)");
     }
 }
