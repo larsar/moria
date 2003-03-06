cd00e163ba0c3d0c108a75576939ed3b09357154
Lars Preben Sorsdahl <lars@preben.no>
2003-03-06 18:08:23 +0000

    Initial revision.
    Example of how to use Mellon. The filter is ready to use with other
    servlets. Just plug it in and you've got FEIDE-authentication.

---
+++ moria/src/no/feide/mellon/filter/AuthenticationFilter.java
@@ -0,0 +1,137 @@
+package no.feide.mellon.servlet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.net.*;
+import no.feide.mellon.Moria;
+import no.feide.mellon.MoriaException;
+import java.util.HashMap;
+
+
+/**
+ * @author Lars Preben S. Arnesen, l.p.arnesen@usit.uio.no
+ * @version $Id$
+ */
+
+
+public class AuthenticationFilter implements Filter {
+  
+    private FilterConfig config = null;
+    
+
+    /** Initialize config. */
+    public void init(FilterConfig config) throws ServletException {
+        this.config = config;
+    }
+    
+    /** Remove config. */
+    public void destroy() {
+        config = null;
+    }
+    
+    /** Perform authentication. If a requests already belongs to a
+     * session and the session contains user data, the user alrady has
+     * been authenticated and are let through. If no user data is
+     * present, the user is redirected to FEIDE for login. After the
+     * user returns from FEIDE, user data will be fetched from Moria
+     * (FEIDE) and stored in the HttpSession. Servlets or other filter
+     * (for instance an authentication filter, may use these data for
+     * user authorization. */
+    // TODO: Error page from config
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        /* The API for Moria (the FEIDE authentication service). */
+        Moria moria;
+
+        try {
+            moria = Moria.getInstance();
+        }
+        
+        catch (MoriaException e) {
+            throw new ServletException(e);
+        }
+
+        /* The HttpSession is used for user tracking so that the user
+         * don't have to authenticate more than once per "surfing
+         * session".*/
+        HttpSession httpSession = 
+            ((HttpServletRequest)request).getSession(true);
+
+        /* The URL parameter used to identify the user after being
+         * sent back from FEIDE. */
+        String moriaID = request.getParameter("moriaID");
+        
+        /* If moriaID is null, the user is either not authenticated
+         * has been authenticated on an earlier request. */
+        if (moriaID == null) {
+
+            /* userData is null when the user is NOT authenticated */ 
+            if (httpSession.getAttribute("userData") == null) {
+              
+                HttpServletRequest httpRequest = (HttpServletRequest) request;
+                String redirectURL; 
+
+                /* Establish contact with Moria and aquire a login
+                 * session. The user should be redirected to this
+                 * URL. */
+                try {
+                    redirectURL = moria.requestSession(new String[] {"cn", "uid"}, httpRequest.getRequestURL().toString()+"?moriaID=", "");
+                }
+                
+                catch (MoriaException e) {
+                    throw new ServletException(e);
+                }
+                
+                /* Redirect to login page */
+                ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);  
+                ((HttpServletResponse)response).setHeader("Location", redirectURL);
+
+            }
+            
+            /* User data is present in the HttpSession. This indicates
+             * that the user has been authenticated earlier and the
+             * request should pass through this filter without any
+             * other actions taken.*/
+            else {
+                chain.doFilter(request, response); 
+            }
+        }
+
+        /* moriaID is present in the request. The user has been
+           redirected from FEIDE (after authentication) } and we
+           should retrieve data about the user and store it in the
+           HttpSession for later authentication (whitout redirecting
+           the user).*/
+        else {
+
+            HashMap attributes;
+
+            /* Fetch user attributes from Moria. */
+            try {
+                attributes = moria.getAttributes(moriaID);
+            }
+
+            catch (MoriaException e) {
+                throw new ServletException(e);
+            }
+
+            /* The user has NOT been authenticated. This should never
+             * be the case since only authenticated users are
+             * redirected from the login service. */
+            if (attributes == null) {
+                // Redirect to  error page.
+            }
+
+            /* We got user data. Store it in the HttpSession for later
+               authentication. The user is authenticated, let him pass. */
+            else {
+                httpSession.setAttribute("userData", attributes);
+            
+                chain.doFilter(request, response);
+            }
+        }
+        
+    }
+}
