6a56854e2b1e466e3a05ed39ee6237ce17a89ab2
Lars Preben Sorsdahl <lars@preben.no>
2003-03-10 14:54:49 +0000

    Added Demo servlet. Should be used together with the AuthenticationFilter. The Servlet displays the user data received from Moria.

---
+++ moria/src/no/feide/mellon/servlet/DemoServlet.java
@@ -0,0 +1,80 @@
+package no.feide.mellon.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * A simple servlet to be used to demonstrate the use of the
+ * AuthenticationFilter.
+ */
+public class DemoServlet extends HttpServlet {
+    
+    /**
+     * A get request only results in the generation of a simple HTML
+     * page. If the AuthenticationFilter has been used to filter all
+     * requests to this servlet, the user has been authenticated by
+     * the FEIDE login service (Moria).
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+
+        /* Fetch userdata from the HttpSession. */
+        HttpSession httpSession = request.getSession(true);
+        HashMap userData = (HashMap) httpSession.getAttribute("userData");
+
+        
+        /* HTML header */
+        out.println("<HTML>");
+        out.println("<HEAD>");
+        out.println("<TITLE>Mellon demo</TITLE>");
+        out.println("</HEAD>");
+        out.println("<BODY>");
+
+        /* If userData == null the user has not been authenticated.
+         * This should not happen. */
+        if (userData == null) {
+            out.println("<H1>NOT authenticated!</H1>");
+            out.println("The user has not been authenticated. The AuthenticationFilter is probably disabled. This should not happen if the filter is configured correct.");
+        }
+
+        /* The user has been authenticated. */
+        else {
+            out.println("<H1>Access granted</H1>");
+        }
+
+        /* The user has been authenticated, but the userData hash is
+         * empty. */
+        if (userData.size() == 0) {
+            out.println("The user has been authenticated, but no more user data is available.");
+        }
+
+        /* Create a table with the contents of the userData hash. */
+        else {
+            out.println("<B>The following data is now stored in the HttpSession:</B></BR></BR>");
+            
+            out.println("<TABLE border=1>");
+            out.println("<TR><TH>Key</TH><TH>Value</TH></TR>");
+
+            for (Iterator iterator = userData.keySet().iterator(); iterator.hasNext();) {
+                String key = (String) iterator.next();
+                out.println("<TR><TD>"+key+"</TD><TD>"+userData.get(key)+"</TD></TR>");
+            }
+
+            out.println("</TABLE>");
+        }
+
+        out.println("</BODY>");
+        out.println("</HTML>");
+        
+    }
+}
