e0cd5a486d0c8382c67b550f4b21cb87cec575e5
Cato Olsen <>
2003-03-25 16:13:09 +0000

    - Now with SSL support against LDAP through LIMS.

--- moria/src/no/feide/moria/User.java
+++ moria/src/no/feide/moria/User.java
@@ -20,7 +20,6 @@ import java.io.File;
 /**
  * Represents a user in the backend. Used to authenticate users and retrieve
  * the associated attributes.
- * @author Cato Olsen
  */
 public class User {
     
@@ -32,13 +31,9 @@ public class User {
      * <code>authenticate(String, String)</code>. */
     private static InitialLdapContext ldap;
     
-    /**
-     * Used to store the user element's relative DN, as found by
-     * <code>ldapSearch(String)</code>.
-     */
-    private static String rdn;
-    
-    
+    /** Used to store the user element's relative DN. */
+    private static String rdn;  
+       
     /**
      * Constructor. Used internally in
      * <code>authenticate(String, String)</code> to return an authenticated
@@ -48,7 +43,6 @@ public class User {
         this.ldap = ldap;
         this.rdn = rdn;
     }
- 
     
     /**
      * Initializes the connection to the backend, and authenticates the user
@@ -86,46 +80,37 @@ public class User {
              (username.length() == 0) || (password.length() == 0) )
             return null;
         
-        // Get and verify properties.
-
+        // Get and verify some properties.
         String keyStore = System.getProperty("no.feide.moria.backend.ldap.keystore");
-        if (keyStore != null)
+        if (keyStore != null) {
             log.config("Key store is "+keyStore);
-        String keyStorePassword = System.getProperty("no.feide.moria.backend.ldap.keystorePassword", null);
+            System.setProperty("javax.net.ssl.keyStore", keyStore);
+        }
+        String keyStorePassword = System.getProperty("no.feide.moria.backend.ldap.keystorePassword");
+        if (keyStorePassword != null)
+                System.setProperty("javax.net.ssl.keyStorePassword", keyStorePassword); 
         String trustStore = System.getProperty("no.feide.moria.backend.ldap.trustStore");
-        if (trustStore != null)
+        if (trustStore != null) {
             log.config("Trust store is "+trustStore);
-        String trustStorePassword = System.getProperty("no.feide.moria.backend.ldap.trustStorePassword", null);
-        String usernameAttribute = System.getProperty("no.feide.moria.backend.ldap.usernameAttribute", null);
+            System.setProperty("javax.net.ssl.trustStore", trustStore);
+        }
+        String trustStorePassword = System.getProperty("no.feide.moria.backend.ldap.trustStorePassword");
+        if (trustStorePassword != null)
+                System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword);
+        String usernameAttribute = System.getProperty("no.feide.moria.backend.ldap.usernameAttribute");
         if (usernameAttribute == null)
             throw new BackendException("Required property no.feide.moria.backend.ldap.usernameAttribute not set");
         log.config("User name attribute is "+usernameAttribute);
-	String ldapURL = System.getProperty("no.feide.moria.backend.ldap.url", null);
+	String ldapURL = System.getProperty("no.feide.moria.backend.ldap.url");
 	if (ldapURL == null)
 	    throw new BackendException("Required property no.feide.moria.backend.ldap.url not set");
         log.config("LDAP URL is "+ldapURL);
-        
-        // Only enable SSL if we have an ldaps:// URL.
-        Hashtable env = new Hashtable();
-
-        if ( (ldapURL.startsWith("ldaps")) ||
-	     (ldapURL.startsWith("ldaps")) ) {
-
-            log.config("SSL enabled");
-            env.put(Context.SECURITY_PROTOCOL, "ssl");
-            Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-
-            System.setProperty("javax.net.ssl.trustStore", trustStore);
-            System.setProperty("javax.net.ssl.trustStorePassword", trustStorePassword);
-            System.setProperty("javax.net.ssl.keyStore", keyStore);
-	    System.setProperty("javax.net.ssl.keyStorePassword", keyStorePassword); 
-        }
-        else
-            log.config("SSL disabled");
-        
+               
+        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
         try {
-
+            
             // Prepare LDAP context and look up user element.
+            Hashtable env = new Hashtable();
             env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
             env.put(Context.PROVIDER_URL, ldapURL);
             env.put(Context.REFERRAL, "throw");  // To catch referrals.
@@ -171,14 +156,11 @@ public class User {
      */
     private static String ldapSearch(String pattern)
     throws BackendException {
-
         log.finer("ldapSearch(String)");
         
         try {
             
-            log.info("DEBUG a");
             NamingEnumeration results = ldap.search("", pattern, new SearchControls(SearchControls.SUBTREE_SCOPE, 1, 0, new String[] {}, false, true));
-            log.info("DEBUG b");
             try {
                 
                 // Nothing matched the pattern.
@@ -189,9 +171,15 @@ public class User {
                 
             } catch (LdapReferralException e) {
                 
-                // We just caught a referral. Follow it recursively.
-                Hashtable refEnv = e.getReferralContext().getEnvironment();
-                log.fine("Matched "+pattern+" on "+ldap.getEnvironment().get(Context.PROVIDER_URL)+" to referral "+refEnv.get(Context.PROVIDER_URL));
+                // We just caught a referral. Follow it recursively, enabling
+                // SSL.
+                log.info("Enabling SSL");
+                Hashtable refEnv = new Hashtable();
+                refEnv.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+                refEnv.put(Context.REFERRAL, "throw");  // To catch referrals.
+                refEnv.put(Context.SECURITY_PROTOCOL, "ssl");
+                refEnv = e.getReferralContext(refEnv).getEnvironment();
+                log.info("Matched "+pattern+" on "+ldap.getEnvironment().get(Context.PROVIDER_URL)+" to referral "+refEnv.get(Context.PROVIDER_URL));
                 ldap.close();
                 ldap = new InitialLdapContext(refEnv, null);
                 return ldapSearch(pattern);
@@ -202,7 +190,7 @@ public class User {
             SearchResult entry = (SearchResult)results.next();
             //assert entry != null : entry;  // Sanity check.
             String rdn = entry.getName();
-            log.fine("Matched "+pattern+" on "+ldap.getEnvironment().get(Context.PROVIDER_URL)+" to element "+rdn);
+            log.info("Matched "+pattern+" on "+ldap.getEnvironment().get(Context.PROVIDER_URL)+" to element "+rdn);
             return rdn;
             
         } catch (NamingException e) {
