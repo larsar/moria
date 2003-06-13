642680a80986dece3bbb0b140dcefeebe5adb464
Cato Olsen <>
2003-06-13 11:27:34 +0000

    Changed hard-coded logout URL back to login page (should really be a logout page, but..)

--- moria/src/no/feide/mellon/servlet/DemoServlet.java
+++ moria/src/no/feide/mellon/servlet/DemoServlet.java
@@ -56,7 +56,7 @@ public class DemoServlet extends HttpServlet {
         if (request.getParameter("logout") != null) {
             httpSession.removeAttribute("userData");
             ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);  
-            ((HttpServletResponse)response).setHeader("Location", "http://demo.feide.no:8080/");
+            ((HttpServletResponse)response).setHeader("Location", request.getRequestURL().toString());
         }
 
         
