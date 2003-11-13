7218d05d5ca649a1ef7ac3a1957ed6ca8ea24ea1
Lars Preben Sorsdahl <lars@preben.no>
2003-11-13 11:14:50 +0000

    Moved from moria project.

---
+++ mellon-java/jaxrpc/no/feide/mellon/filter/AuthenticationFilter.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright (C) 2003 FEIDE
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package no.feide.mellon.filter;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import no.feide.mellon.Moria;
+import no.feide.mellon.MoriaException;
+
+
+/**
+ * @author Lars Preben S. Arnesen, l.p.arnesen@usit.uio.no
+ * @version $Id$
+ */
+
+
+public class AuthenticationFilter implements Filter {
+  
+    private FilterConfig config = null;
+
+    /** Initialize config. */
+    public void init(FilterConfig config) throws ServletException {
+        this.config = config;
+
+	// Read Mellon-side properties.
+	try {
+            System.getProperties().load(getClass().getResourceAsStream("/mellon.properties"));
+	} catch (IOException e) {
+	    throw new ServletException("IOException caught and re-thrown as ServletException");
+	}        
+    }
+    
+    /** Remove config. */
+    public void destroy() {
+        config = null;
+    }
+    
+    /** Perform authentication. If a requests already belongs to a
+     * session and the session contains user data, the user alrady has
+     * been authenticated and are let through. If no user data is
+     * present, the user is redirected to FEIDE for login. After the
+     * user returns from FEIDE, user data will be fetched from Moria
+     * (FEIDE) and stored in the HttpSession. Servlets or other filter
+     * (for instance an authentication filter, may use these data for
+     * user authorization. */
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        /* The API for Moria (the FEIDE authentication service). */
+        Moria moria;
+
+        try {
+            moria = Moria.getInstance();
+        }
+        
+        catch (MoriaException e) {
+            throw new ServletException(e);
+        }
+
+        /* The HttpSession is used for user tracking so that the user
+         * don't have to authenticate more than once per "surfing
+         * session".*/
+        HttpSession httpSession = 
+            ((HttpServletRequest)request).getSession(true);
+
+        /* The URL parameter used to identify the user after being
+         * sent back from FEIDE. */
+        String moriaID = request.getParameter("moriaID");
+        
+        /* If moriaID is null, the user is either not authenticated
+         * has been authenticated on an earlier request. */
+        if (moriaID == null) {
+
+            /* userData is null when the user is NOT authenticated */ 
+            if (httpSession.getAttribute("userData") == null) {
+                
+                HttpServletRequest httpRequest = (HttpServletRequest) request;
+                String redirectURL; 
+
+                /* Construct the URL that Moria should redirect the
+                 * user back to after authentication. */
+                String requestURL = httpRequest.getRequestURL().toString();
+                String backToMellonURL  = requestURL;
+
+                if (httpRequest.getQueryString() != null) 
+                    backToMellonURL += "?"+httpRequest.getQueryString()+"&moriaID=";
+                else
+                    backToMellonURL += "?moriaID=";
+
+                /* Establish contact with Moria and aquire a login
+                 * session. The user should be redirected to this
+                 * URL. */
+                try {
+                     redirectURL = moria.requestSession(System.getProperty("no.feide.mellon.requestedAttributes").split(","), backToMellonURL, "", false);
+                }
+                
+                catch (MoriaException e) {
+                    throw new ServletException(e);
+                }
+                
+                /* Redirect to login page */
+                ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);  
+                ((HttpServletResponse)response).setHeader("Location", redirectURL);
+
+            }
+            
+            /* User data is present in the HttpSession. This indicates
+             * that the user has been authenticated earlier and the
+             * request should pass through this filter without any
+             * other actions taken.*/
+            else {
+                chain.doFilter(request, response); 
+            }
+        }
+
+        /* moriaID is present in the request. The user has been
+           redirected from FEIDE (after authentication) } and we
+           should retrieve data about the user and store it in the
+           HttpSession for later authentication (whitout redirecting
+           the user).*/
+        else {
+
+            HashMap attributes;
+
+            /* Fetch user attributes from Moria. */
+            try {
+                attributes = moria.getAttributes(moriaID);
+            }
+
+            /* The user has NOT been authenticated. This should never
+             * be the case since only authenticated users are
+             * redirected from the login service. */
+            catch (MoriaException e) {
+                ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_FORBIDDEN);  
+                return;
+            }
+
+
+            /* We got user data. Store it in the HttpSession for later
+               authentication. The user is authenticated, let him pass. */
+            httpSession.setAttribute("userData", attributes);
+            chain.doFilter(request, response);
+
+        }
+        
+    }
+}
---
+++ mellon-java/jaxrpc/no/feide/mellon/servlet/DemoServlet.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (C) 2003 FEIDE
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package no.feide.mellon.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * A simple servlet to be used to demonstrate the use of the
+ * AuthenticationFilter.
+ */
+public class DemoServlet extends HttpServlet {
+    
+    /**
+     * A get request only results in the generation of a simple HTML
+     * page. If the AuthenticationFilter has been used to filter all
+     * requests to this servlet, the user has been authenticated by
+     * the FEIDE login service (Moria).
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+
+
+
+        /* Fetch userdata from the HttpSession. */
+        HttpSession httpSession = request.getSession(true);
+        HashMap userData = (HashMap) httpSession.getAttribute("userData");
+
+
+        /* Logout */
+        if (request.getParameter("logout") != null) {
+            httpSession.removeAttribute("userData");
+            ((HttpServletResponse)response).setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);  
+            ((HttpServletResponse)response).setHeader("Location", request.getRequestURL().toString());
+        }
+
+        
+        /* HTML header */
+        out.println("<HTML>");
+        out.println("<HEAD>");
+        out.println("<TITLE>Mellon demo</TITLE>");
+        out.println("</HEAD>");
+        out.println("<BODY>");
+
+        /* If userData == null the user has not been authenticated.
+         * This should not happen. */
+        if (userData == null) {
+            out.println("<H1>NOT authenticated!</H1>");
+            out.println("The user has not been authenticated. The AuthenticationFilter is probably disabled. This should not happen if the filter is configured correct.");
+            return;
+        }
+
+        /* The user has been authenticated. */
+        else {
+            out.println("<H1>Access granted</H1>");
+        }
+
+        /* Logout */
+        out.println("[<a href=\""+request.getRequestURL().toString()+"?logout"+"\">Logout</a>]<BR>");
+
+
+        /* The user has been authenticated, but the userData hash is
+         * empty. */
+        if (userData.size() == 0) {
+            out.println("The user has been authenticated, but no more user data is available.");
+        }
+
+        /* Create a table with the contents of the userData hash. */
+        else {
+            /* Logout link */
+
+            out.println("<B>You have been authenticated and the following attributes are now stored in the HttpSession of the web service:</B></BR></BR>");
+            
+            out.println("<TABLE border=1>");
+            out.println("<TR><TH>Key</TH><TH>Value</TH></TR>");
+
+            for (Iterator iterator = userData.keySet().iterator(); iterator.hasNext();) {
+                String key = (String) iterator.next();
+                out.println("<TR><TD>"+key+"</TD><TD>"+userData.get(key)+"</TD></TR>");
+            }
+
+            out.println("</TABLE>");
+        }
+
+        out.println("</BODY>");
+        out.println("</HTML>");
+        
+    }
+}
