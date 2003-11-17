6b961e3b895df3b007c2ffac84cc87cb69a6ee1e
Cato Olsen <>
2003-11-17 14:18:03 +0000

    Fixed bad package declarations.

--- mellon-java/jaxrpc/src/no/feide/mellon/MoriaException.java
+++ mellon-java/jaxrpc/src/no/feide/mellon/MoriaException.java
@@ -15,7 +15,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-package jaxrpc.no.feide.mellon;
+package no.feide.mellon;
 
 /**
  * Used to signal an exception from the Moria interface.
