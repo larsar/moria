d00ee04e321aa91a98cab8eaa3135c8369fa7375
Cato Olsen <>
2003-12-01 15:51:30 +0000

    Moria client test application, currently pretty much hard coded...

---
+++ mellon-java/jaxrpc/src/no/feide/mellon/jaxrpc/test/MellonTest.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (C) 2003 FEIDE
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package no.feide.mellon.jaxrpc.test;
+
+import javax.xml.rpc.Stub;
+import no.feide.moria.service.Attribute;
+import no.feide.moria.service.AuthenticationIF;
+import no.feide.moria.service.Authentication_Impl;
+
+
+/**
+ * Moria client test application. 
+ */
+public class MellonTest {
+
+	/** A valid URL prefix. */
+	private static final String urlPrefix = "http://www.feide.no?id=";
+
+	/** A valid URL postfix. */
+	private static final String urlPostfix = "";
+	
+
+	/**
+	 * The main (and only) method.
+	 * @param args Ignored.
+	 */
+	public static void main(String[] args) {
+		
+		System.err.println("Starting");
+		try {
+	
+			// Create service proxy and set credentials.
+			Stub stub = (Stub)(new Authentication_Impl().getAuthenticationIFPort());
+			stub._setProperty(javax.xml.rpc.Stub.USERNAME_PROPERTY, "demo");
+			stub._setProperty(javax.xml.rpc.Stub.PASSWORD_PROPERTY, "demo");
+			System.err.println("Client authentication: demo/demo");
+	
+			// Request a new session.
+			AuthenticationIF service = (AuthenticationIF)stub;
+			String[] attributeRequest = {"eduPersonAffiliation", "eduPersonOrgDN"};
+			System.err.println("Session request:  {eduPersonAffiliation, eduPersonOrgDN}");
+	        String sessionID = service.requestSession(attributeRequest, urlPrefix, urlPostfix, false);
+			sessionID = sessionID.substring(sessionID.indexOf("id=")+3);
+			System.err.println("Session established: "+sessionID);
+	        
+			// Authenticate.
+			System.err.println("User authentication: test@uninett.no/test");
+			sessionID = service.authenticateUser(sessionID, "test@uninett.no", "test");
+			System.err.println("User authenticated: "+sessionID);
+	
+			// Get and display user attributes.
+			System.err.println("Requesting attributes");
+	        Attribute[] attributes = service.getAttributes(sessionID);
+	        for (int i=0; i<attributes.length; i++) {
+	            String[] values = attributes[i].getValues();
+	            for (int j=0; j<values.length; j++)
+					System.err.println('\t'+attributes[i].getName()+": "+values[j]);
+	        }
+	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.err.println("Finished");
+
+
+	}
+	
+}
