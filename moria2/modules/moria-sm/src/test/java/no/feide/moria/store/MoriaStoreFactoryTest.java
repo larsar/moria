c53cb71d448a86496c56192637a6088fb64e5cf0
Bjorn Ola Smievoll <b.o@smievoll.no>
2004-02-03 19:08:32 +0000

    Initial revision

---
+++ moria2/modules/moria-sm/src/test/java/no/feide/moria/store/MoriaStoreFactoryTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2004 FEIDE
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place - Suite 330, Boston, MA 02111-1307, USA.
+ * 
+ * $Id$
+ */
+
+package no.feide.moria.store;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * @author Bjørn Ola Smievoll &lt;b.o@smievoll.no&gt;
+ * @version $Revision$
+ */
+public class MoriaStoreFactoryTest extends TestCase {
+    public MoriaStoreFactoryTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(MoriaStoreFactoryTest.class);
+    }
+
+    /**
+     * Simple test that verifies the creation of a MoriaStore instance as the
+     * result of the method tested. 
+     */
+    public void testCreateMoriaStore() {
+        MoriaStore store = MoriaStoreFactory.createMoriaStore();
+        assertTrue(store instanceof MoriaStore);
+    }
+}
