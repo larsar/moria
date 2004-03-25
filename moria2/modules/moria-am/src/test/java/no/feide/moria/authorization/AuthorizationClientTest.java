e11ce43fe2c4807c634205578c316cd1a79a3978
Lars Preben Sorsdahl <lars@preben.no>
2004-03-25 08:39:48 +0000

    Corrected comment

--- moria2/modules/moria-am/src/test/java/no/feide/moria/authorization/AuthorizationClientTest.java
+++ moria2/modules/moria-am/src/test/java/no/feide/moria/authorization/AuthorizationClientTest.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2004 FEIDE
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -229,13 +229,8 @@ public class AuthorizationClientTest extends TestCase {
         } catch (IllegalArgumentException success) {
         }
 
-        try {
-            client.allowAccessTo(new String[]{});
-            fail("Should raise IllegalArgumentException, empty string array");
-        } catch (IllegalArgumentException success) {
-        }
-
-        /* SSO for attributes */
+        /* Access to attributes */
+        Assert.assertTrue("Should get access (no attributes)", client.allowAccessTo(new String[]{}));
         Assert.assertTrue("Should get access", client.allowAccessTo(new String[]{"attr1"}));
         Assert.assertTrue("Should get access", client.allowAccessTo(new String[]{"attr1", "attr2"}));
         Assert.assertFalse("Should not get access", client.allowAccessTo(new String[]{"attr2", "attr3"}));
