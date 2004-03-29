f6213d49f1c0495bc47e4636c0471844fb576243
Bjorn Ola Smievoll <b.o@smievoll.no>
2004-03-29 18:44:19 +0000

    Fixed several problems in generateLogMessage()

--- moria2/modules/moria-log/src/java/no/feide/moria/log/MessageLogger.java
+++ moria2/modules/moria-log/src/java/no/feide/moria/log/MessageLogger.java
@@ -1,31 +1,37 @@
 /*
  * Copyright (c) 2004 UNINETT FAS
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place - Suite 330, Boston, MA 02111-1307, USA.
- * 
+ *
  * $Id$
- *  
+ * 
  */
 
 package no.feide.moria.log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 /**
  * @author Bjørn Ola Smievoll &lt;b.o@smievoll.no&gt;
  * @version $Revision$
  */
 public final class MessageLogger extends Logger {
+
+    private final static String LOGGER_NAME = MessageLogger.class.getName();
 
     /**
      * Default private constructor
@@ -91,7 +97,7 @@ public final class MessageLogger extends Logger {
      *            the message to log
      */
     public static void logWarn(final String message) {
-        log4j.warn(generateLogMessage(message, null ,null));
+        log4j.warn(generateLogMessage(message, null, null));
     }
 
     /**
@@ -235,7 +241,7 @@ public final class MessageLogger extends Logger {
     }
 
     /**
-     * Generate the final string to give to the underlying log api.
+     * Generate the final log entry to give to the underlying log api
      * 
      * @param message
      *            the message to log
@@ -251,26 +257,18 @@ public final class MessageLogger extends Logger {
         StringBuffer buffer = new StringBuffer();
 
         buffer.append(ticketId != null ? "[" + ticketId + "] " : "[-] ");
-        buffer.append(message != null ? "\"message\"" : "\"-\"");
+        buffer.append(message != null ? "\"" + message + "\"" : "\"-\"" + System.getProperty("line.separator"));
 
         if (exception != null) {
-            /*
-             * Append message int exception on own line.
-             */
-            buffer.append(System.getProperty("line.separator"));
-            buffer.append(exception.getMessage());
-            buffer.append(System.getProperty("line.separator"));
 
-            /*
-             * Append string representation of every element in the exception
-             * stack.
-             */
-            StackTraceElement[] stackTrace = exception.getStackTrace();
+            /* Capture stacktrace */
+            OutputStream outputStream = new ByteArrayOutputStream();
+            PrintStream printStream = new PrintStream(outputStream);
 
-            for (int i = 0; i < stackTrace.length; i++) {
-                buffer.append(stackTrace[i].toString());
-                buffer.append(System.getProperty("line.seperator"));
-            }
+            exception.printStackTrace(printStream);
+            printStream.flush();
+
+            buffer.append(outputStream.toString() + System.getProperty("line.separator"));
         }
         return buffer.toString();
     }
