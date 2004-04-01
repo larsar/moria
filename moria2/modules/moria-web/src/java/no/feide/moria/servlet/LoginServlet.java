28b58eb35d9a72771aaf4a7c67444d664f7ab81f
Lars Preben Sorsdahl <lars@preben.no>
2004-04-01 14:57:41 +0000

    Multiple changes for testing purposes.

--- moria2/modules/moria-web/src/java/no/feide/moria/servlet/LoginServlet.java
+++ moria2/modules/moria-web/src/java/no/feide/moria/servlet/LoginServlet.java
@@ -25,7 +25,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.ServletException;
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import java.io.IOException;
+import java.util.TreeMap;
+import java.util.ResourceBundle;
+import java.util.Properties;
+import java.util.StringTokenizer;
+import java.util.HashMap;
+
+import no.feide.moria.controller.MoriaController;
 
 /**
  * @author Lars Preben S. Arnesen &lt;lars.preben.arnesen@conduct.no&gt;
@@ -33,19 +41,50 @@ import java.io.IOException;
  */
 public class LoginServlet extends HttpServlet {
 
+
     /**
      * Handles the GET requests.
      *
-     * @param request           the HTTP request object
-     * @param response          the HTTP response object
+     * @param request  the HTTP request object
+     * @param response the HTTP response object
      * @throws IOException
      * @throws ServletException
      */
     public final void doGet(final HttpServletRequest request, final HttpServletResponse response)
             throws IOException, ServletException {
-        request.setAttribute("foo", "bar");
+        // TODO: Do not throw exceptions, set INTERNAL SERVER ERRROR status
+        String jspLocation = getServletContext().getInitParameter("jsp.location");
 
-        RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.jsp");
+        MoriaController.initController(getServletConfig().getServletContext());
+
+        // TODO: Lots of dummy stuff
+        ResourceBundle bundle = RequestUtil.getBundle("login", null, null, null, null, "en");
+        request.setAttribute("organizationNames", RequestUtil.organizationNames(getConfig(), bundle.getLocale().getLanguage()));
+        request.setAttribute("bundle", bundle);
+        request.setAttribute("selectedRealm", "uninett.no");
+
+        RequestDispatcher rd = getServletContext().getRequestDispatcher(jspLocation + "/login.jsp");
         rd.include(request, response);
     }
+
+
+    private Properties getConfig() {
+        Properties config;
+
+        /* Validate config */
+        try {
+            ServletContext context = getServletConfig().getServletContext();
+            config = (Properties) getServletConfig().getServletContext().getAttribute("config");
+        } catch (ClassCastException e) {
+            // TODO: Log
+            throw new IllegalStateException("Config is not correctly set in context.");
+        }
+        if (config == null) {
+            // TODO: Log
+            throw new IllegalStateException("Config is not set in context.");
+        }
+
+        return config;
+    }
+
 }
