02a2914e917988b2a9c7989a29193bbfa5d35791
Bjorn Ola Smievoll <b.o@smievoll.no>
2004-04-30 16:07:17 +0000

    Formating and javadoc updates

--- moria2/modules/moria-log/src/test/java/no/feide/moria/log/MessageLoggerTest.java
+++ moria2/modules/moria-log/src/test/java/no/feide/moria/log/MessageLoggerTest.java
@@ -32,15 +32,19 @@ import junit.framework.TestSuite;
  */
 public class MessageLoggerTest extends TestCase {
 
+    /** The logger used by the tests */
     private MessageLogger messageLogger;
 
+    /** PRNG to generate pseudo tickets */
     private Random random;
 
+    /** Ceiling for PRNG */
     private final int maxRandomNumber = 999999999;
 
     /**
-     * 
-     * @return
+     * Create a test suite of all tests in class. 
+     *
+     * @return a suite of all tests in class
      */
     public static Test suite() {
         return new TestSuite(MessageLoggerTest.class);
@@ -60,7 +64,7 @@ public class MessageLoggerTest extends TestCase {
     }
 
     /**
-     * Class to test for void logCritical(String).
+     * Method to test: void logCritical(String).
      */
     public final void testLogCriticalString() {
         messageLogger.logCritical(null);
@@ -68,56 +72,71 @@ public class MessageLoggerTest extends TestCase {
     }
 
     /**
-     * Class to test for void logCritical(String, Exception).
+     * Method to test: void logCritical(String, Exception).
      */
     public final void testLogCriticalStringException() {
         String message = null;
         Exception exception = null;
+
         messageLogger.logCritical(message, exception);
 
+        /* message */
         message = "Testing logCritical(message, exception)";
+
         messageLogger.logCritical(message, exception);
 
+        /* exception */
         exception = new Exception("Test exception");
-        messageLogger.logCritical(null, exception);
 
+        messageLogger.logCritical(null, exception);
         messageLogger.logCritical(message, exception);
     }
 
     /**
-     * Class to test for void logCritical(String, String).
+     * Method to test: void logCritical(String, String).
      */
     public final void testLogCriticalStringString() {
         String message = null;
         String ticketId = null;
+
         messageLogger.logCritical(message, ticketId);
 
+        /* message */
         message = "Testing logCritical(message, ticketid)";
+
         messageLogger.logCritical(message, ticketId);
 
+        /* ticketId */
         ticketId = new Integer(random.nextInt(maxRandomNumber)).toString();
-        messageLogger.logCritical(null, ticketId);
 
+        messageLogger.logCritical(null, ticketId);
         messageLogger.logCritical(message, ticketId);
     }
 
     /**
-     * Class to test for void logCritical(String, String, Exception).
+     * Method to test: void logCritical(String, String, Exception).
      */
     public final void testLogCriticalStringStringException() {
         String message = null;
         String ticketId = null;
         Exception exception = null;
+
         messageLogger.logCritical(message, ticketId, exception);
 
+        /* message */
         message = "Testing logCritical(message, ticketid, exception)";
+
         messageLogger.logCritical(message, ticketId, exception);
 
+        /* ticketId */
         ticketId = new Integer(random.nextInt(maxRandomNumber)).toString();
+
         messageLogger.logCritical(null, ticketId, exception);
         messageLogger.logCritical(message, ticketId, exception);
 
+        /* exception */
         exception = new Exception("Test exception");
+
         messageLogger.logCritical(null, null, exception);
         messageLogger.logCritical(null, ticketId, exception);
         messageLogger.logCritical(message, null, exception);
@@ -125,7 +144,7 @@ public class MessageLoggerTest extends TestCase {
     }
 
     /**
-     * Class to test for void logWarn(String).
+     * Method to test: void logWarn(String).
      */
     public final void testLogWarnString() {
         messageLogger.logWarn(null);
@@ -133,56 +152,71 @@ public class MessageLoggerTest extends TestCase {
     }
 
     /**
-     * Class to test for void logWarn(String, Exception).
+     * Method to test: void logWarn(String, Exception).
      */
     public final void testLogWarnStringException() {
         String message = null;
         Exception exception = null;
+
         messageLogger.logWarn(message, exception);
 
+        /* message */
         message = "Testing logWarn(message, exception)";
+
         messageLogger.logWarn(message, exception);
 
+        /* exception */
         exception = new Exception("Test exception");
-        messageLogger.logWarn(null, exception);
 
+        messageLogger.logWarn(null, exception);
         messageLogger.logWarn(message, exception);
     }
 
     /**
-     * Class to test for void logWarn(String, String).
+     * Method to test: void logWarn(String, String).
      */
     public final void testLogWarnStringString() {
         String message = null;
         String ticketId = null;
+
         messageLogger.logWarn(message, ticketId);
 
+        /* message */
         message = "Testing logWarn(message, ticketid)";
+
         messageLogger.logWarn(message, ticketId);
 
+        /* ticketId */
         ticketId = new Integer(random.nextInt(maxRandomNumber)).toString();
-        messageLogger.logWarn(null, ticketId);
 
+        messageLogger.logWarn(null, ticketId);
         messageLogger.logWarn(message, ticketId);
     }
 
     /**
-     * Class to test for void logWarn(String, String, Exception).
+     * Method to test: void logWarn(String, String, Exception).
      */
     public final void testLogWarnStringStringException() {
         String message = null;
         String ticketId = null;
         Exception exception = null;
+
         messageLogger.logWarn(message, ticketId, exception);
 
+        /* message */
         message = "Testing logWarn(message, ticketid, exception)";
+
         messageLogger.logWarn(message, ticketId, exception);
 
+        /* ticketId */
         ticketId = new Integer(random.nextInt(maxRandomNumber)).toString();
+
         messageLogger.logWarn(null, ticketId, exception);
         messageLogger.logWarn(message, ticketId, exception);
 
+        /* exception */
         exception = new Exception("Test exception");
+
         messageLogger.logWarn(null, null, exception);
         messageLogger.logWarn(null, ticketId, exception);
         messageLogger.logWarn(message, null, exception);
@@ -190,7 +224,7 @@ public class MessageLoggerTest extends TestCase {
     }
 
     /**
-     * Class to test for void logInfo(String).
+     * Method to test: void logInfo(String).
      */
     public final void testLogInfoString() {
         messageLogger.logInfo(null);
@@ -198,56 +232,71 @@ public class MessageLoggerTest extends TestCase {
     }
 
     /**
-     * Class to test for void logInfo(String, Exception).
+     * Method to test: void logInfo(String, Exception).
      */
     public final void testLogInfoStringException() {
         String message = null;
         Exception exception = null;
+
         messageLogger.logInfo(message, exception);
 
+        /* message */
         message = "Testing logInfo(message, exception)";
+
         messageLogger.logInfo(message, exception);
 
+        /* exception */
         exception = new Exception("Test exception");
-        messageLogger.logInfo(null, exception);
 
+        messageLogger.logInfo(null, exception);
         messageLogger.logInfo(message, exception);
     }
 
     /**
-     * Class to test for void logInfo(String, String).
+     * Method to test: void logInfo(String, String).
      */
     public final void testLogInfoStringString() {
         String message = null;
         String ticketId = null;
+
         messageLogger.logInfo(message, ticketId);
 
+        /* message */
         message = "Testing logInfo(message, ticketid)";
+
         messageLogger.logInfo(message, ticketId);
 
+        /* ticketId */
         ticketId = new Integer(random.nextInt(maxRandomNumber)).toString();
-        messageLogger.logInfo(null, ticketId);
 
+        messageLogger.logInfo(null, ticketId);
         messageLogger.logInfo(message, ticketId);
     }
 
     /**
-     * Class to test for void logInfo(String, String, Exception).
+     * Method to test: void logInfo(String, String, Exception).
      */
     public final void testLogInfoStringStringException() {
         String message = null;
         String ticketId = null;
         Exception exception = null;
+
         messageLogger.logInfo(message, ticketId, exception);
 
+        /* message */
         message = "Testing logInfo(message, ticketid, exception)";
+
         messageLogger.logInfo(message, ticketId, exception);
 
+        /* ticketId */
         ticketId = new Integer(random.nextInt(maxRandomNumber)).toString();
+
         messageLogger.logInfo(null, ticketId, exception);
         messageLogger.logInfo(message, ticketId, exception);
 
+        /* exception */
         exception = new Exception("Test exception");
+
         messageLogger.logInfo(null, null, exception);
         messageLogger.logInfo(null, ticketId, exception);
         messageLogger.logInfo(message, null, exception);
@@ -255,7 +304,7 @@ public class MessageLoggerTest extends TestCase {
     }
 
     /**
-     * Class to test for void logDebug(String).
+     * Method to test: void logDebug(String).
      */
     public final void testLogDebugString() {
         messageLogger.logDebug(null);
@@ -263,41 +312,49 @@ public class MessageLoggerTest extends TestCase {
     }
 
     /**
-     * Class to test for void logDebug(String, Exception).
+     * Method to test: void logDebug(String, Exception).
      */
     public final void testLogDebugStringException() {
         String message = null;
         Exception exception = null;
+
         messageLogger.logDebug(message, exception);
 
+        /* message */
         message = "Testing logDebug(message, exception)";
+
         messageLogger.logDebug(message, exception);
 
+        /* exception */
         exception = new Exception("Test exception");
-        messageLogger.logDebug(null, exception);
 
+        messageLogger.logDebug(null, exception);
         messageLogger.logDebug(message, exception);
     }
 
     /**
-     * Class to test for void logDebug(String, String).
+     * Method to test: void logDebug(String, String).
      */
     public final void testLogDebugStringString() {
         String message = null;
         String ticketId = null;
+
         messageLogger.logDebug(message, ticketId);
 
+        /* message */
         message = "Testing logDebug(message, ticketid)";
+
         messageLogger.logDebug(message, ticketId);
 
+        /* ticketId */
         ticketId = new Integer(random.nextInt(maxRandomNumber)).toString();
-        messageLogger.logDebug(null, ticketId);
 
+        messageLogger.logDebug(null, ticketId);
         messageLogger.logDebug(message, ticketId);
     }
 
     /**
-     * Class to test for void logDebug(String, String, Exception).
+     * Method to test: void logDebug(String, String, Exception).
      */
     public final void testLogDebugStringStringException() {
         String message = null;
@@ -305,14 +362,20 @@ public class MessageLoggerTest extends TestCase {
         Exception exception = null;
         messageLogger.logDebug(message, ticketId, exception);
 
+        /* message */
         message = "Testing logDebug(message, ticketid, exception)";
+
         messageLogger.logDebug(message, ticketId, exception);
 
+        /* ticketId */
         ticketId = new Integer(random.nextInt(maxRandomNumber)).toString();
+
         messageLogger.logDebug(null, ticketId, exception);
         messageLogger.logDebug(message, ticketId, exception);
 
+        /* exception */
         exception = new Exception("Test exception");
+
         messageLogger.logDebug(null, null, exception);
         messageLogger.logDebug(null, ticketId, exception);
         messageLogger.logDebug(message, null, exception);
