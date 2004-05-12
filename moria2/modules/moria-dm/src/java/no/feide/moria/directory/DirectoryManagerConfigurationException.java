4f0ebe150c1ebb02d3b86bd33b6a52114f11a87d
Cato Olsen <>
2004-05-12 12:49:28 +0000

    Initial revision.

---
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/DirectoryManagerConfigurationException.java
@@ -0,0 +1,12 @@
+package no.feide.moria.directory;
+
+/**
+ * @author Cato Olsen Used to signal an exception related to the directory
+ *         manager's configuration.
+ */
+public class DirectoryManagerConfigurationException
+extends DirectoryManagerException {
+
+    // Does nothing.
+
+}
