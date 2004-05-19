3a92c13bd922e84a1c3c1e466ee7f06f1374d9c5
Bjorn Ola Smievoll <b.o@smievoll.no>
2004-05-19 09:51:36 +0000

    Code style clean up

--- moria2/modules/moria-sm/src/java/no/feide/moria/store/CachedUserData.java
+++ moria2/modules/moria-sm/src/java/no/feide/moria/store/CachedUserData.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2004 FEIDE
- * 
+ * Copyright (c) 2004 UNINETT FAS
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place - Suite 330, Boston, MA 02111-1307, USA.
- * 
+ *
  * $Id$
  */
 
@@ -26,28 +26,30 @@ import java.util.HashMap;
 /**
  * This class is the container for the userdata that persist across logins (for
  * SSO-Light and Proxy-authentication).
- * 
+ *
  * @author Bjørn Ola Smievoll &lt;b.o@smievoll.no&gt;
  * @version $Revision$
  */
-public class CachedUserData implements Serializable {
+public final class CachedUserData implements Serializable {
 
+    /** The internal hashmap used to store the attributes. */
     private final HashMap attributes;
 
     /**
-     * Constructs a new instance encapsulating the userdata given as argument
-     * 
-     * @param attributes a hashmap containing the user attributes
+     * Constructs a new instance encapsulating the userdata given as argument.
+     *
+     * @param attributes
+     *          a hashmap containing the user attributes
      */
-    public CachedUserData(HashMap attributes) throws IllegalArgumentException {
+    public CachedUserData(final HashMap attributes) {
         if (attributes == null)
             throw new IllegalArgumentException("Argument can not be null");
         this.attributes = attributes;
     }
 
     /**
-     * Get a map containing the attributes
-     * 
+     * Get a map containing the attributes.
+     *
      * @return a clone of the internal attribute map
      */
     public HashMap getAttributes() {
