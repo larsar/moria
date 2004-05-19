7582ca4b271a3d470664f97526cb85d8a5f54da2
Bjorn Ola Smievoll <b.o@smievoll.no>
2004-05-19 08:36:23 +0000

    Checkstyle cleanup

--- moria2/modules/moria-sm/src/java/no/feide/moria/store/InvalidTicketException.java
+++ moria2/modules/moria-sm/src/java/no/feide/moria/store/InvalidTicketException.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (c) 2004 FEIDE
- * 
+ * Copyright (c) 2004 UNINETT FAS
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place - Suite 330, Boston, MA 02111-1307, USA.
- * 
+ *
  * $Id$
  */
 
@@ -22,7 +22,7 @@ package no.feide.moria.store;
 
 /**
  * This exception should be thrown whenever a ticket is considered erroneous.
- * 
+ *
  * @author Bjørn Ola Smievoll &lt;b.o@smievoll.no&gt;
  * @version $Revision$
  */
@@ -38,22 +38,21 @@ public class InvalidTicketException extends Exception {
     /**
      * @see java.lang.Exception#Exception(java.lang.String)
      */
-    public InvalidTicketException(String message) {
+    public InvalidTicketException(final String message) {
         super(message);
     }
 
     /**
-     * @see java.lang.Exception#Exception(java.lang.String,
-     *      java.lang.Throwable)
+     * @see java.lang.Exception#Exception(java.lang.String, java.lang.Throwable)
      */
-    public InvalidTicketException(String message, Throwable cause) {
+    public InvalidTicketException(final String message, final Throwable cause) {
         super(message, cause);
     }
 
     /**
      * @see java.lang.Exception#Exception(java.lang.Throwable)
      */
-    public InvalidTicketException(Throwable cause) {
+    public InvalidTicketException(final Throwable cause) {
         super(cause);
     }
 }
