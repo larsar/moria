e79126229ac6eebac8407c18cdefd2d07b86dd78
Bjorn Ola Smievoll <b.o@smievoll.no>
2004-05-19 08:49:52 +0000

    Updated comments and code style

--- moria2/modules/moria-sm/src/java/no/feide/moria/store/MoriaStoreFactory.java
+++ moria2/modules/moria-sm/src/java/no/feide/moria/store/MoriaStoreFactory.java
@@ -1,33 +1,48 @@
 /*
- * Copyright (c) 2004 FEIDE
- * 
+ * Copyright (c) 2004 UNINETT FAS
+ *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place - Suite 330, Boston, MA 02111-1307, USA.
- * 
+ *
  * $Id$
  */
 
 package no.feide.moria.store;
 
-
 /**
+ * Class implementing simple factory pattern returning implementation
+ * of MoriaStore interface.  Somewhat redundant as we only have one
+ * implemenetation at the moment.
+ *
  * @author Bjørn Ola Smievoll &lt;b.o@smievoll.no&gt;
  * @version $Revision$
  */
-public class MoriaStoreFactory {
+public final class MoriaStoreFactory {
 
+    /**
+     * Default private constructor.
+     */
+    private MoriaStoreFactory() {
+    }
+
+    /**
+     * Static factory method creates an instance of MoriaStore.
+     *
+     * @return instance of MoriaStore implementation
+     */
     public static MoriaStore createMoriaStore() {
+        // TODO: Read class to instantiate from config
         return new MoriaCacheStore();
     }
 }
