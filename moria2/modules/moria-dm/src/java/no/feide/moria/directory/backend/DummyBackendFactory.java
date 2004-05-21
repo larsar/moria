093000929cb37e8b7d6fa9e2a87a072763534351
Cato Olsen <>
2004-05-21 11:24:51 +0000

    Removed constructor.

--- moria2/modules/moria-dm/src/java/no/feide/moria/directory/backend/DummyBackendFactory.java
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/backend/DummyBackendFactory.java
@@ -8,16 +8,6 @@ public class DummyBackendFactory
 implements DirectoryManagerBackendFactory {
 
     /**
-     * Constructor. Does nothing.
-     */
-    public DummyBackendFactory() {
-
-        super();
-        
-    }
-
-
-    /**
      * Creates a new <code>DummyBackend</code> instance.
      * @see no.feide.moria.directory.backend.DirectoryManagerBackendFactory#createBackend()
      */
