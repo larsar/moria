465d6ca4be434069986d5ab58d26fd47a3c04164
Cato Olsen <>
2004-05-21 11:12:27 +0000

    Fixed constructor.

--- moria2/modules/moria-dm/src/java/no/feide/moria/directory/UserAttribute.java
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/UserAttribute.java
@@ -7,22 +7,45 @@ import java.util.Vector;
  */
 public class UserAttribute {
 
+    /** Internal representation of the attribute's name. */
+    private String myName;
+
     /** Internal representation of the attribute's values. */
     private Vector myValues;
 
 
     /**
      * Constructor.
+     * @param name
+     *            The attribute name.
      * @param values
      *            A list of attribute values. A <code>null</code> value is
      *            accepted, as is an empty array.
+     * @throws IllegalAttributeException
+     *             If the attribute name is <code>null<code> or an empty string.
      */
-    public UserAttribute(String[] values) {
+    public UserAttribute(String name, String[] values)
+    throws IllegalAttributeException {
+
+        if ((name == null) || (name.length() == 0)) {
+        // TODO: Add logging.
+        throw new IllegalAttributeException("Attribute name cannot be NULL"); }
 
         myValues = new Vector();
         if (values != null)
             for (int i = 0; i < values.length; i++)
                 myValues.add(values[i]);
+
+    }
+
+
+    /**
+     * Get the attribute's name.
+     * @return A new string containing the attribute's name.
+     */
+    public String getName() {
+
+        return new String(myName);
 
     }
 
