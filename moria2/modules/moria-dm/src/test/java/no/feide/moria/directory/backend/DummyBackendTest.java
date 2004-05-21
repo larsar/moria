82e72ba76b4866d26a1b39252d5bff2eee19e1d1
Cato Olsen <>
2004-05-21 13:52:54 +0000

    Added more testing.

--- moria2/modules/moria-dm/src/test/java/no/feide/moria/directory/backend/DummyBackendTest.java
+++ moria2/modules/moria-dm/src/test/java/no/feide/moria/directory/backend/DummyBackendTest.java
@@ -54,7 +54,10 @@ extends TestCase {
         
         // Prepare.
         Credentials goodCredentials = new Credentials("test@feide.no", "test");
+        Credentials[] badCredentials = {new Credentials("test@feide.nO", "test"), new Credentials("test@feide.no", "Test"), null};
         String[] goodRequest = {"eduPersonAffiliation"};
+        String[][] noRequests = {null, {""}};
+        String[] badRequest = {"someNonExistingAttribute"};
         String[] goodValues = {"Affiliate"};
         UserAttribute goodAttribute = null;
         try {
@@ -64,9 +67,42 @@ extends TestCase {
         }
         Assert.assertNotNull("User attribute was not instantiated", goodAttribute);
         UserAttribute[] goodAttributes = {goodAttribute};
+       
+        // Test unsuccessful authentication.
+        for (int i=0; i<badCredentials.length; i++)
+	        try {
+	            backend.authenticate(badCredentials[i], goodRequest);
+	            Assert.fail("Bad authentication succeeded");
+	        } catch (AuthenticationFailedException e) {
+	            // Expected.
+	        } catch (BackendException e) {
+	            Assert.fail("Unexpected BackendException");
+	        }
         
-        // Test successful authentication.
+        // Test successful authentication, without requested attributes.
+	    for (int i=0; i<noRequests.length; i++)
+	        try {
+	        	backend.authenticate(goodCredentials, noRequests[i]);
+	        } catch (AuthenticationFailedException e) {
+	            Assert.fail("Authentication failed (AuthenticationFailedException)");
+	        } catch (BackendException e) {
+	            Assert.fail("Unexpected BackendException");
+	        }
+	        
+	    // Test successful authentication, with non-existing requested attributes.
         UserAttribute[] attributes = null;
+        try {
+        	attributes = backend.authenticate(goodCredentials, badRequest);
+        } catch (AuthenticationFailedException e) {
+            Assert.fail("Authentication failed (AuthenticationFailedException)");
+        } catch (BackendException e) {
+            Assert.fail("Unexpected BackendException");
+        }
+        Assert.assertNotNull("No attributes returned", attributes);
+        Assert.assertEquals("Non-existing attributes returned after authentication", 0, attributes.length);
+        
+        // Test successful authentication, with requested attributes.
+        attributes = null;
         try {
         	attributes = backend.authenticate(goodCredentials, goodRequest);
         } catch (AuthenticationFailedException e) {
@@ -74,8 +110,6 @@ extends TestCase {
         } catch (BackendException e) {
             Assert.fail("Unexpected BackendException");
         }
-        
-        // Check returned attribute values.
         Assert.assertNotNull("No attributes returned", attributes);
     	Assert.assertEquals("Unexpected number of attributes returned after authentication", goodAttributes.length, attributes.length);
     	String[] values = attributes[0].getValues();
