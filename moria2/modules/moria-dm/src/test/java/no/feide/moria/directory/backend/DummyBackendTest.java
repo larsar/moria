427656553580e8635048888cb6c217a5c2b1136a
Cato Olsen <>
2004-05-21 13:27:07 +0000

    Initial revision.

---
+++ moria2/modules/moria-dm/src/test/java/no/feide/moria/directory/backend/DummyBackendTest.java
@@ -0,0 +1,87 @@
+package no.feide.moria.directory.backend;
+
+import junit.framework.*;
+import no.feide.moria.directory.Credentials;
+import no.feide.moria.directory.IllegalAttributeException;
+import no.feide.moria.directory.UserAttribute;
+
+/**
+ * JUnit tests for the <code>DummyBackend</code> class.
+ */
+public class DummyBackendTest
+extends TestCase {
+    
+    /** Internal representation of the backend. */
+    private DummyBackend backend;
+
+    /**
+     * Returns the full test suite.
+     * @return The test suite.
+     */
+    public static Test suite() {
+
+        return new TestSuite(DummyBackendTest.class);
+        
+    }
+    
+    /**
+     * Prepare.
+     */
+    public void setUp() {
+        
+        backend = new DummyBackend();
+        Assert.assertNotNull("Failed to instantiate backend object", backend);
+        // Should call open(), but does nothing.
+
+    }
+
+
+    /**
+     * Clean up.
+     */
+    public void tearDown() {
+
+        // Should call close(), but does nothing.
+        backend = null;
+        
+    }
+    
+    
+    /**
+     * 
+     */
+    public void testAuthenticate() {
+        
+        // Prepare.
+        Credentials goodCredentials = new Credentials("test@feide.no", "test");
+        String[] goodRequest = {"eduPersonAffiliation"};
+        String[] goodValues = {"Affiliate"};
+        UserAttribute goodAttribute = null;
+        try {
+            goodAttribute = new UserAttribute("eduPersonAffiliation", goodValues);
+        } catch (IllegalAttributeException e) {
+            Assert.fail("Unexpected IllegalAttributeException");
+        }
+        Assert.assertNotNull("User attribute was not instantiated", goodAttribute);
+        UserAttribute[] goodAttributes = {goodAttribute};
+        
+        // Test successful authentication.
+        UserAttribute[] attributes = null;
+        try {
+        	attributes = backend.authenticate(goodCredentials, goodRequest);
+        } catch (AuthenticationFailedException e) {
+            Assert.fail("Authentication failed (AuthenticationFailedException)");
+        } catch (BackendException e) {
+            Assert.fail("Unexpected BackendException");
+        }
+        
+        // Check returned attribute values.
+        Assert.assertNotNull("No attributes returned", attributes);
+    	Assert.assertEquals("Unexpected number of attributes returned after authentication", goodAttributes.length, attributes.length);
+    	String[] values = attributes[0].getValues();
+    	Assert.assertEquals("Unexpected number of attribute values returned after authentication", values.length, goodValues.length);
+    	Assert.assertEquals("Attribute values doesn't match", values[0], goodValues[0]);
+        
+    }
+
+}
