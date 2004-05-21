19f415913392d54fe77c14e5ea6ca48a0f876945
Cato Olsen <>
2004-05-21 10:37:39 +0000

    Renamed DirectoryManagerBackendException.

---
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/backend/BackendException.java
@@ -0,0 +1,35 @@
+package no.feide.moria.directory.backend;
+
+import no.feide.moria.directory.DirectoryManagerException;
+
+/**
+ * Represents an exception originating form the directory manager's backend.
+ */
+public class BackendException
+extends DirectoryManagerException {
+
+    /**
+     * Constructor.
+     * @param message
+     *            The exception message.
+     */
+    public BackendException(String message) {
+
+        super(message);
+
+    }
+    
+    /**
+     * Constructor.
+     * @param message
+     *            The exception message.
+     * @param cause
+     *            The exception cause.
+     */
+    public BackendException(String message, Throwable cause) {
+
+        super(message, cause);
+
+    }
+
+}
