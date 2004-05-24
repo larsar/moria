8e610f8dd90d3434c4c0eb743a03f1a3cd2754af
Cato Olsen <>
2004-05-24 15:59:05 +0000

    Removed Register. RegisterManager may also disappear.

--- moria2/modules/moria-dm/src/java/no/feide/moria/directory/index/RegisterManager.java
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/index/RegisterManager.java
@@ -1,36 +1,20 @@
-/*
- * Copyright (c) 2004 UNINETT FAS A/S
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place - Suite 330, Boston, MA 02111-1307, USA.
- */
 package no.feide.moria.directory.index;
+
+import java.util.HashMap;
 
 /**
  * Manages the internal representation of affiliations; separate sets for user
  * and group affiliations.
- * @author Cato Olsen
  */
 public class RegisterManager {
 
     private static RegisterManager me = null;
 
-    private static Register users;
+    private static HashMap users;
 
-    private static Register groups;
+    private static HashMap groups;
 
-    private static Register affiliations;
+    private static HashMap affiliations;
 
 
     /**
@@ -38,9 +22,9 @@ public class RegisterManager {
      * affiliations.
      */
     private RegisterManager() {
-        users = new Register();
-        groups = new Register();
-        affiliations = new Register();
+        users = new HashMap();
+        groups = new HashMap();
+        affiliations = new HashMap();
     }
 
 
@@ -59,7 +43,7 @@ public class RegisterManager {
      * Get the list of user affiliations.
      * @return A reference to the user affiliation object.
      */
-    public static Register getusers() {
+    public static HashMap getusers() {
         return users;
     }
 
@@ -68,7 +52,7 @@ public class RegisterManager {
      * Get the list of group affiliations.
      * @return A reference to the group affiliation object.
      */
-    public static Register getgroups() {
+    public static HashMap getgroups() {
         return groups;
     }
 
@@ -76,7 +60,7 @@ public class RegisterManager {
     /**
      * @return
      */
-    public static Register getAffiliations() {
+    public static HashMap getAffiliations() {
         return affiliations;
     }
 
