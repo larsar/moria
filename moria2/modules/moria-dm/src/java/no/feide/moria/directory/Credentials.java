c0f0a7209e0de5db22591e3c1264d78e03be4c18
Cato Olsen <>
2004-05-24 13:04:12 +0000

    Added sanity check on creation.

--- moria2/modules/moria-dm/src/java/no/feide/moria/directory/Credentials.java
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/Credentials.java
@@ -16,11 +16,24 @@ public class Credentials {
     /**
      * Constructor. Creates a new set of credentials.
      * @param username
-     *            The username. May be <code>null</code>.
+     *            The username. May not be <code>null</code> or an empty
+     *            string.
      * @param password
-     *            The user's password. May be <code>null</code>.
+     *            The user's password. May not be <code>null</code> or an
+     *            empty string.
+     * @throws IllegalCredentialsException
+     *             If attempting to create a set of credentials where either the
+     *             username or password is <code>null</code> or an empty
+     *             string.
      */
-    public Credentials(String username, String password) {
+    public Credentials(String username, String password)
+    throws IllegalCredentialsException {
+
+        // Sanity checks.
+        if ((username == null) || (username.length() == 0))
+            throw new IllegalCredentialsException("Illegal username");
+        if ((password == null || password.length() == 0))
+            throw new IllegalCredentialsException("Illegal password");
 
         this.username = username;
         this.password = password;
