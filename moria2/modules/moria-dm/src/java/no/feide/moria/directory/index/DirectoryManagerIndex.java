d805c08c36f0a5995c7d1b450c0d09dd04498bc5
Cato Olsen <>
2004-05-27 14:10:27 +0000

    Support for more than one reference per id.

--- moria2/modules/moria-dm/src/java/no/feide/moria/directory/index/DirectoryManagerIndex.java
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/index/DirectoryManagerIndex.java
@@ -1,5 +1,7 @@
 package no.feide.moria.directory.index;
 
+import java.util.List;
+
 /**
  * The interface to an underlying index implementation.
  */
@@ -9,9 +11,9 @@ public interface DirectoryManagerIndex {
      * Looks up the backend reference from a given logical ID.
      * @param id
      *            The logical ID.
-     * @return The backend reference, or <code>null</code> if no such
+     * @return A list of backend references, or <code>null</code> if no such
      *         reference was found.
      */
-    public String lookup(String id);
+    public List lookup(String id);
 
 }
