d0ca749344c50876d97469a92589db4ca250f7c1
Cato Olsen <>
2004-05-28 11:37:55 +0000

    No longer a subclass of BackendException.

--- moria2/modules/moria-dm/src/java/no/feide/moria/directory/backend/AuthenticationFailedException.java
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/backend/AuthenticationFailedException.java
@@ -1,10 +1,12 @@
 package no.feide.moria.directory.backend;
 
+import no.feide.moria.directory.DirectoryManagerException;
+
 /**
  * Used to signal a failed authentication attempt.
  */
 public class AuthenticationFailedException
-extends BackendException {
+extends DirectoryManagerException {
 
     /**
      * Constructor.
