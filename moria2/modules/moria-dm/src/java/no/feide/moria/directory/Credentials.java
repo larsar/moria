9ae0be566b82f7d39be6c34b18e6a98340d1a2c1
Cato Olsen <>
2004-05-28 14:10:09 +0000

    Elaborated JavaDoc.

--- moria2/modules/moria-dm/src/java/no/feide/moria/directory/Credentials.java
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/Credentials.java
@@ -1,12 +1,16 @@
 package no.feide.moria.directory;
 
 /**
- * Represents a set of user credentials - for now, only username/password, which
- * is a dumbed-down version of the more general mechanism in Moria 1.
+ * Represents a set of user credentials, that is, a username/password pair. Used
+ * for Moria authentication methods, and may be expanded to support other types
+ * of credentials in a future version <br>
+ * <br>
+ * Note that this is a subset of the functionality offered by the Moria 1
+ * <code>Credentials</code> class.
  */
 public class Credentials {
 
-    /** Internal representation of the username . */
+    /** Internal representation of the username. */
     private final String username;
 
     /** Internal representation of the user's password. */
@@ -14,13 +18,17 @@ public class Credentials {
 
 
     /**
-     * Constructor. Creates a new set of credentials.
+     * Constructor. Creates a new set of credentials consisting of a
+     * username/password pair.
      * @param username
      *            The username. May not be <code>null</code> or an empty
      *            string.
      * @param password
      *            The user's password. May not be <code>null</code> or an
      *            empty string.
+     * @throws IllegalArgumentException
+     *             If either <code>username</code> or <code>password</code>
+     *             is <code>null</code> or an empty string.
      */
     public Credentials(final String username, final String password) {
 
@@ -38,7 +46,7 @@ public class Credentials {
 
     /**
      * Retrieve the username part of the credentials.
-     * @return A new <code>String</code> containing the user name.
+     * @return A newly allocated <code>String</code> containing the username.
      */
     public String getUsername() {
 
@@ -49,7 +57,7 @@ public class Credentials {
 
     /**
      * Retrieve the password part of the credentials.
-     * @return A new <code>String</code> containing the password.
+     * @return A newly allocated <code>String</code> containing the password.
      */
     public String getPassword() {
 
