c31fb7d9c7fe19aa96378be6b7182c879d21c7f3
Cato Olsen <>
2004-06-01 17:03:18 +0000

    Now uses DummyBackend.setConfig(Element).

--- moria2/modules/moria-dm/src/java/no/feide/moria/directory/backend/DummyBackendFactory.java
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/backend/DummyBackendFactory.java
@@ -2,22 +2,48 @@ package no.feide.moria.directory.backend;
 
 import java.util.Properties;
 
+import no.feide.moria.directory.DirectoryManagerConfigurationException;
+
+import org.jdom.Element;
+
 /**
  * Factory class for dummy backends.
  */
 public class DummyBackendFactory
 implements DirectoryManagerBackendFactory {
-    
-    
+
+    /** Holds the dummy backend configuration. */
+    private Element myConfig;
+
+
     /**
-     * Does nothing.
-     * @see no.feide.moria.directory.backend.DirectoryManagerBackend#setConfig(java.util.Properties)
+     * Configure use of the dummy backends. May be called repeatedly to update
+     * used configuration. <br>
+     * <br>
+     * Note that much of the parsing of the configuration element is done in the
+     * <code>DummyBackend</code> class.
+     * @param config
+     *            The new or updated configuration for the dummy backend. Cannot
+     *            be <code>null</code>.
+     * @throws IllegalArgumentException
+     *             If <code>config</code> is <code>null</code>.
+     * @throws DirectoryManagerConfigurationException
+     *             If <code>config</code> is not a <code>Backend</code>
+     *             element.
+     * @see DirectoryManagerBackendFactory#setConfig(Properties)
      */
-    public void setConfig(Properties config) {
+    public void setConfig(Element config) {
+
+        // Sanity checks.
+        if (config == null)
+            throw new IllegalArgumentException("Backend configuration element cannot be NULL");
+        if (!config.getName().equalsIgnoreCase("Backend"))
+            throw new DirectoryManagerConfigurationException("Cannot find backend configuration element");
         
-        // Does nothing.
-        
+        myConfig = (Element)config.clone();
+
     }
+
 
     /**
      * Creates a new <code>DummyBackend</code> instance.
@@ -25,7 +51,9 @@ implements DirectoryManagerBackendFactory {
      */
     public DirectoryManagerBackend createBackend() {
 
-        return new DummyBackend();
+        DummyBackend newBackend = new DummyBackend();
+        newBackend.setConfig(myConfig);
+        return newBackend;
 
     }
 
