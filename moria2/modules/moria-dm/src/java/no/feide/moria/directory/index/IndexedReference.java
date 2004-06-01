c79525c17550a1fb5c07a4ff1cb2e088fa11cfae
Cato Olsen <>
2004-06-01 12:38:04 +0000

    Initial revision. Used to differ between explicitly and implicitly indexed elements.

---
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/index/IndexedReference.java
@@ -0,0 +1,78 @@
+package no.feide.moria.directory.index;
+
+/**
+ * Represents an indexed reference to an external element. Used to differ
+ * between explicitly and implicitly indexed references.
+ */
+public class IndexedReference {
+
+    private final String myName;
+
+    private final String[] myReferences;
+
+    private final boolean explicit;
+
+
+    /**
+     * Constructor. Creates a new indexed reference.
+     * @param name
+     *            The index reference's name.
+     * @param references
+     *            One or more external references. Cannot be <code>null</code>.
+     * @param explicitReference
+     *            <code>true</code> if this is a fully qualified reference to
+     *            an external element, otherwise <code>false</code>.
+     */
+    public IndexedReference(String name, String[] references, boolean explicitReference) {
+
+        super();
+
+        // Sanity checks.
+        if (name == null)
+            throw new IllegalArgumentException("Name cannot be NULL");
+        if ((references == null) || (references.length == 0))
+            throw new IllegalArgumentException("References cannot be NULL or an empty string");
+
+        // Assignments.
+        myName = name;
+        myReferences = references;
+        explicit = explicitReference;
+
+    }
+
+
+    /**
+     * Get the index reference's name.
+     * @return The index reference's name.
+     */
+    public String getName() {
+
+        return myName;
+
+    }
+
+
+    /**
+     * Get the external references.
+     * @return One or more external references.
+     */
+    public String[] getReferences() {
+
+        return myReferences;
+
+    }
+
+
+    /**
+     * Check whether this reference is an explicit reference to an external
+     * element, or an implicit reference to a search base of some sort.
+     * @return <code>true</code> if this is a fully qualified external
+     *         reference to an element, otherwise <code>false</code>.
+     */
+    public boolean isExplicitlyIndexed() {
+
+        return explicit;
+
+    }
+
+}
