f27f098f3edc4210986a7e58e9c13f464ca7c9d5
Cato Olsen <>
2004-06-01 11:00:26 +0000

    Added the setConfig method.

--- moria2/modules/moria-dm/src/java/no/feide/moria/directory/backend/DirectoryManagerBackendFactory.java
+++ moria2/modules/moria-dm/src/java/no/feide/moria/directory/backend/DirectoryManagerBackendFactory.java
@@ -4,10 +4,22 @@
  */
 package no.feide.moria.directory.backend;
 
+import java.util.Properties;
+
 /**
  * The interface to the backend factory.
  */
 public interface DirectoryManagerBackendFactory {
+
+    /**
+     * Set the backend factory configuration. Must be called before
+     * <code>createBackend()</code> is used.
+     * @param config
+     *            The backend configuration. See details for the actual backend
+     *            implementation used.
+     */
+    public void setConfig(Properties config);
+
 
     /**
      * Creates a new instance of a proper directory manager backend.
